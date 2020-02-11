238bd45c141b45c137f4caa6bc13c51cbdd3d45b
kaushik <kaushikkumar004@gmail.com>
2020-02-11 22:48:30 +0530

    initial commit

---
+++ kaushik_epam_calci/src/main/java/epam/kaushik_epam_calci/App.java
@@ -0,0 +1,17 @@
+package epam.kaushik_epam_calci;
+
+import frontpart.Window;
+//import java.awt.Window;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Window w = new Window();
+        w.setVisible(true);
+    }
+}
